9ef339535379d2872e707763dd98c1859b95760b
Abiolayoung <abiolayoung229@gmail.com>
2024-01-07 01:28:28 +0100

    update codes

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+// function allow us do some javascript operations and use them multiple times when needed
+
+// function delcaration
+
+// keyword name()
+// function addition(){
+//     console.log(3+5)
+// }
+
+// function subtraction(){
+//     console.log(6-4)
+// }
+// addition()
+// subtraction()
+
+// function argument or function parameters- it allows user to pass in parameter that want to be executed
+// function addition(a,b){
+//     console.log(a+b)
+// }
+// addition(10,3)
+
+// function subtraction(a,b){
+//     console.log(a-b)
+// }
+// subtraction(10,3)
+
+// function showMessage(){
+//     let msg = 'i love javascript'
+//     console.log(msg);
+// }
+// showMessage()
+
+// function showDiscount(purchase = prompt('enter your purchase amount')){
+//     if(!purchase){
+//         alert('enter your purchase amount')
+//     }
+//     if(purchase>=1500){
+//         alert('congratulation! you have 15% discount applied to your purchase')
+//     } else if(purchase<1500){
+//         alert('kindly make purchase of over 1500')
+//     }
+// }
+// showDiscount()
+
+// local variable are variable that are declared inside of a function, and cannot be accessed outside of the function scope
+
+
+
+
+
 
